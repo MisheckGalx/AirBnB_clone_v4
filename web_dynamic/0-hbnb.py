963db6a1bf0eb9ff9eaf2c0f78a2fd2d04e249e0
misheck gogo <misheckgogo45@gmail.com>
2024-05-03 15:20:43 -0700

    Write a script that starts a Flask web application:Cash only

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ Starts a Flask Web Application """
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.place import Place
+from flask import Flask, render_template
 from uuid import uuid4
 
+
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -17,24 +20,24 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    states = sorted(storage.all(State).values(), key=lambda k: k.name)
     st_ct = [[state, sort_cities_by_name(state)] for state in states]
-    amenities = sorted(storage.all(Amenity).values(), key=lambda x: x.name)
-    places = sorted(storage.all(Place).values(), key=lambda x: x.name)
+
+    amenities = sorted(storage.all(Amenity).values(), key=lambda k: k.name)
+    places = sorted(storage.all(Place).values(), key=lambda k: k.name)
 
     # Generate a UUID and convert it to a string
     cache_id = str(uuid4())
 
-    return render_template(
-        '0-hbnb.html',
-        states=st_ct,
-        amenities=amenities,
-        places=places,
-        cache_id=cache_id
-    )
+    return render_template('100-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           users=users,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
